844efda762853683f8527afbec6c420172a88798
Mashrafi <gaintakwa@gmail.com>
2023-01-22 06:56:47 +0600

    yea commit

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       //No no
+       //No no  just jj
     }
 }
